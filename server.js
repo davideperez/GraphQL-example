96a7e921e2bcb7dbc7f3019d50bfda3ad3168843
David Perez <david.ezequiel.pere@gmail.com>
2023-03-22 14:49:46 -0300

    Implemented GraphiQL, replaced graphql-http dependency with graphql-yoga

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express  = require('express');
-const { buildSchema } = require('graphql');
-const { graphqlHTTP } = require('express-graphql');
+const { createYoga } = require('graphql-yoga');
+const { createSchema } = require('graphql-yoga');
 
-const schema = buildSchema(`
-    type Query {
+const schema = createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
         description: String
         price: Float
-    }
-`);
+      }
+    `,
+    resolvers: {
+      Query: {
+        description: () => 'Red Shoe',
+        price: () => 42.12,
 
-const root = {
-    description: 'RedShoe',
-    price: 42.12,
-}
+      }
+    }
+  })
 
 const app = express()
 
+const yoga = createYoga({
+    schema,
+    context: (req) => ({ // Context factory gets called for every request
+        //myToken: req.headers.get('authorization') // I've commented this line because it was causing problems and it seems to work :)
+    }),
+    graphiql: true,
+})
+
 // A continuacion el endpoint de graphql
-app.use('/graphql', graphqlHTTP({
-    schema: schema, //se indica el schema a express.
-    rootValue: root, //se indica a express, como se va a responder a los querys.
-}))
+app.use('/graphql', yoga)
 
 app.listen(3000, () => {
     console.log('Running GraphQL server...')
